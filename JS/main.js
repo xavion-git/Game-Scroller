ad1a63a5135a9312a443abe8b9e897f0c47c7721
Xavion-git <xdean1@nait.ca>
2025-11-05 12:40:27 -0700

    Added the contorl keys function

--- JS/main.js
+++ JS/main.js
@@ -3,4 +3,17 @@ let score = 0;
 let lives = 3;
 let runningGame = true;
 
+// Making a player object
+const player = {
+    x: 50,          // The position on the screen on the x and y
+    y: 300, 
+    width: 30,      // The width/ hight of the player
+    height: 30, 
+    velocityX: 0,   // The velocity of the player for the scrolling 
+    velocityY: 0, 
+    speed: 5,       // How fast the character
+    jumpPower: 10,  // How high the character can jump
+    onTheGround: false // is the player on the platform
+};
+
 
